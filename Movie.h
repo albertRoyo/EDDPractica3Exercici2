11c6679b876169fb017b44a83d10536110858117
Albert Royo <31955715+albertRoyo@users.noreply.github.com>
2018-05-10 16:07:21 +0200

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -1,1 +1,27 @@
-implement
+#ifndef MOVIE_H
+#define MOVIE_H
+
+#include <string>
+
+class Movie {
+public:
+
+    //Constructors
+    Movie();
+    Movie(int id, string titol, double rating);
+    
+    //Consultors
+    int getId();//potser no serà necessaria
+    string getTitol();
+    double getRating();
+    
+    //Modificadors
+    void setId(int id);
+    void setTitol(string titol);
+    void setRating(double rating);
+    
+    string toString();
+    
+}
+    
+#endif /* MOVIE_H */

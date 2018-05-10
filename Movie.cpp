98cd5c388b5faf7022bb7f081d51d7e23e36f49f
Albert Royo <31955715+albertRoyo@users.noreply.github.com>
2018-05-10 16:06:23 +0200

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -1,1 +1,37 @@
-implement
+#include "Movie.h"
+
+//Constructors
+Movie(){
+}
+Movie(int id, string titol, double rating){
+    this->id = id;
+    this->titol = titol;
+    this->rating = rating;
+}
+
+//Consultors
+int getId(){
+    return this->id;
+}
+string getTitol(){
+    return this->titol;
+}
+double getRating(){
+    return this->rating;
+}
+    
+//Modificadors
+void setId(int id){
+    this->id = id;
+}
+void setTitol(string titol){
+    this->titol = titol;
+}
+void setRating(double rating){
+    this->rating = rating;
+}
+    
+string toString(){
+    return this->getTitol()+toString(this->getRating());
+}
+    

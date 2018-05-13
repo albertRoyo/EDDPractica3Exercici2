5c18089a8613dec193ef1f34da4a83b23de0f205
Albert Royo <31955715+albertRoyo@users.noreply.github.com>
2018-05-13 15:56:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,99 @@
 #include <iostream>
-
-#include "BinarySearchTree.h"
-#include "BinarySearchTree.cpp"
-#include "TreeNode.h"
-#include "TreeNode.cpp"
+#include <vector>
+#include <stdexcept>
+#include <time.h>
+#include <fstream>
+#include "BSTMovieFinder.h"
 
 using namespace std;
-/*
-g++ -std=c++11 $(ls *.cpp) && ./a.out
-g++ -std=c++11 -c BinarySearchTree.cpp
-g++ -std=c++11 -c TreeNode.cpp
-g++ -std=c++11 -o main.exe main.o TreeNode.o BinarySearchTree.o*/
 
-int main() {
-    BinarySearchTree<int>* tree = new BinarySearchTree<int>(); //constructor ok
-    tree->insert(54);
-    tree->insert(23);
-    tree->insert(68); // los inserts funcionan bien
-    tree->insert(10);
-    tree->insert(34);
-    tree->insert(57);
-    tree->insert(123);
+void mostrar_menu(vector<string> vec_options){
+    for (int i = 0; i < vec_options.size(); ++i){
+        cout <<"  "<<i+1<<". "<<vec_options[i]<<endl;
+    }
+}
+
+int main(){    //Usada la estructura switch de la primera pràctica
+    vector<string> vec_options(6);
+    vec_options = {"Inserir fitxer", "Mostrar arbre", "Llegir fitxer de cerca", 
+                   "Mostrar profunditat", "Sortir"};
+    int instruccio;
     
-    cout << "\t\t" << tree->root()->getData() << endl;
-    cout << "\t" << tree->root()->getLeft()->getData() << "\t\t" << tree->root()->getRight()->getData() << endl;
-    cout << "    " << tree->root()->getLeft()->getLeft()->getData();
-    cout << "    " << tree->root()->getLeft()->getRight()->getData();
-    cout << "\t     " << tree->root()->getRight()->getLeft()->getData();
-    cout << "    " << tree->root()->getRight()->getRight()->getData() << endl;
-    cout << endl;
-    
-    //ja funcionan tots, era simplement canviar alguns left per right
-    cout << "Preordre, inordre i postordre:" << endl;
-    tree->printPreorder();
-    tree->printInorder();
-    tree->printPostorder();
-    
-    cout << "Mida: " << tree->size() << endl;
-    cout << "Búsquedes:" << endl;
-    cout << tree->search(23) << endl;
-    cout << tree->search(123) << endl; // los search funcionan bien
-    cout << tree->search(145) << endl;
-    
-    cout << "Alçada:" << tree->getHeight() << endl;// alçada del arbre
-    
-    //(comptant que una fulla té alçada 0)
-    
-    return 0;
+    BSTMovieFinder* movieTree;
+    do {
+        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+        cout << "  Que vols fer?" << endl;
+        mostrar_menu(vec_options);
+        cin >> instruccio;
+        //try{
+            switch (instruccio){
+                case 1:{    //Inserir fitxer
+                    movieTree = new BTSMovieFinder();
+                    cout << "Estructura creada!" << endl;
+                    cout << "Quin fitxer vols afegir(P/G)?" << endl;
+                    char c;
+                    cin >> c;
+                    string nomFitxer;
+                    if (c == 'P'){
+                        nomFitxer = "movie_rating_small.txt";
+                    }
+                    else if (c == 'G'){
+                        nomFitxer = "movie_rating.txt";
+                    }
+                    clock_t start = clock();
+                    movieTree->appendMovies(nomFitxer);
+                    cout << "Arbre inicialitzat amb les dades de '" << nomFitxer << "'." << endl;
+                    cout << "El temps de creació del arbre ha sigut de " << (double)(clock()-start)/CLOCKS_PER_SEC << " segons." << endl;
+                }
+                
+                case 2:{    //Mostrar arbre
+                    vector<Movie> movies = movieTree->arbre->printInorder();
+                    char c = 'y';
+                    int j = 0;
+                    while (c == 'y'){
+                        int k;
+                        for (int i = j; i < j+40 and i < movies.size(); ++i){//Potser trobes una forma millor
+                           cout << movies[i].toString() << endl;
+                           k=i;
+                        }
+                        j=k;
+                        cout << "Vols imprimir les següents 40 pel·lícules(y/n)?" << endl;
+                        cin >> c;
+                    }
+                }
+                    
+                case 3:{    //LLegir fitxer de cerca
+                    ifstream fitxer("cercaPelicules.txt");
+                    int trobades = 0;
+                    int total = 0;
+                    clock_t start = clock();
+                    while (!fitxer.eof()){
+                        ++total;
+                        string sID;
+                        fitxer >> sID;
+                        TreeNode<Movie>* newNode = movieTree->arbre->search(stoi(sID));
+                        if (newNode != nullptr){
+                            ++trobades;
+                        }
+                        delete [] newNode;
+                    }
+                    fitxer.close();
+                    cout << "S'han trobat " << trobades << " películes d'un total de " << total << "." << endl;
+                    cout << "Ha trigat un temps de " << (double)(clock()-start)/CLOCKS_PER_SEC << " segons." << endl;
+                }
+                
+                case 4:{    //Mostrar profunditat
+                    int height = movieTree->arbre->getHeight();
+                    cout << "La profunditat del arbre és " << height << "." << endl;
+                }
+                case 5:{    //Sortir
+                    break;
+                }
+                
+                default:{
+                    cout << "Instrucció incorrecte" << endl;
+                }
+            }
+    }
+    while (instruccio != 5);    //Sortir
 }
